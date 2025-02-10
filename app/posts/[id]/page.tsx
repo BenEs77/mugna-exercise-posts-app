b0dd3f51307ab43aa7c2ba9b71ecd657c6c75139
prince <prince.oncada@gmail.com>
2025-02-09 19:00:18 -0500

    dev: set PostPage as its own server component for initial data fetching

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,5 +1,5 @@
+import PostContent from "@/components/PostContent";
 import { getPostById } from "@/lib/services/postService";
-import Link from "next/link";
 
 interface ParamsProps {
     id: string;
@@ -14,15 +14,12 @@ const PostPage = async ({ params }: PostProps) => {
     const { id } = await params;
     const post = await getPostById(id);
 
+    console.log(post)
+
     return (
         <div className="p-6 bg-gray-100 min-h-screen">
             <div className="max-w-3xl mx-auto bg-white shadow-md rounded p-6">
-                <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
-                <p className="text-gray-600 mb-4">By {post.author}</p>
-                <p className="text-lg">{post.content}</p>
-                <div className="mt-6 flex justify-between">
-                    <Link href="/posts" className="px-4 py-2 bg-gray-600 text-white rounded">Back to Posts</Link>
-                </div>
+                <PostContent post={post} />
             </div>
         </div>
     );
